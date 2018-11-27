5db02826d409533704c729f21c0f6a188110305f
zhangxuefeng <zhangxuefeng@365haoyou.com>
2018-11-27 15:05:56 +0800

    init

---
+++ src/main/java/com/zxf/quartz/MainTest.java
@@ -0,0 +1,14 @@
+package com.zxf.quartz;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainTest {
+
+    public static void main(String[] args) {
+        System.out.println("Test start.");
+        ApplicationContext context = new ClassPathXmlApplicationContext("quartz-job-config.xml");
+        //如果配置文件中将startQuertz bean的lazy-init设置为false 则不用实例化
+        //context.getBean("startQuertz");
+        System.out.print("Test end..");
+    }
+}
---
+++ src/main/java/com/zxf/quartz/QuartzJob.java
@@ -0,0 +1,7 @@
+package com.zxf.quartz;
+
+public class QuartzJob {
+    public void work() {
+        System.out.println("测试Quartz的定时任务调度！！！");
+    }
+}
