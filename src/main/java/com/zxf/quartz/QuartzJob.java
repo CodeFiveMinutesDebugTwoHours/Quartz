6a64f27ce00d64eecef09f6f00d8cc92c44ac30d
zhangxuefeng <zhangxuefeng@365haoyou.com>
2018-11-27 16:37:43 +0800

    dayinshijain

--- src/main/java/com/zxf/quartz/QuartzJob.java
+++ src/main/java/com/zxf/quartz/QuartzJob.java
@@ -1,7 +1,9 @@
 package com.zxf.quartz;
 
+import java.util.Date;
+
 public class QuartzJob {
     public void work() {
-        System.out.println("测试Quartz的定时任务调度！！！");
+        System.out.println("测试Quartz的定时任务调度！！！时间："+new Date());
     }
 }
